da98c7dd15fdd49d1fcff1d17c4f054758c2d3c5
zhuobai <32185512+zhuobai@users.noreply.github.com>
2017-10-08 17:49:13 +0800

    add project

---
+++ newde/app/src/main/java/com/example/del/newde/fragment/FourFragment.java
@@ -0,0 +1,23 @@
+package com.example.del.newde.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.del.newde.R;
+
+/**
+ * Created by del on 2017/10/8.
+ */
+
+public class FourFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View v = inflater.inflate(R.layout.layout_four, container,false);
+        return v;
+    }
+
+}
---
+++ newde/app/src/main/java/com/example/del/newde/fragment/SecondFragment.java
@@ -0,0 +1,22 @@
+package com.example.del.newde.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.del.newde.R;
+
+/**
+ * Created by del on 2017/10/8.
+ */
+
+public class SecondFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View v = inflater.inflate(R.layout.layout_second, container,false);
+        return v;
+    }
+}
